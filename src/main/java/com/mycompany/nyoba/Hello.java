2735e8015f85e9c296a940af12f58b1caf6947c3
dederodhatul <64527665+dederodhatul@users.noreply.github.com>
2020-05-01 13:03:12 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/nyoba/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.nyoba;
+
+/**
+ *
+ * @author HP
+ */
+public class Hello {
+
+    /**
+     *  
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World!!");
+    }
+}
